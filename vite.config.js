b0c96d0e8b68616842f390119802cfa8c5d0cf54
MOHAMMED AADHIL N <aadhil1304@gmail.com>
2026-01-01 12:31:08 +0530

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/aadhi-os-portfolio/", // 👈 EXACT repo name
+  base: "/aadhil-portfolio/", // ✅ MUST MATCH REPO NAME
 });
